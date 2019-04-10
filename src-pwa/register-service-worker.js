4ecf6ef8470eac242903e7766c01f58874261ddb
Severin Hauser <severin@tilted.ch>
2019-04-10 17:54:55 +0200

    fix(serviceworker): disable eslint

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -1,32 +1,27 @@
-/*
- * This file is picked up by the build system only
- * when building for PRODUCTION
- */
+/* eslint-disable */
 
-import { register } from 'register-service-worker'
+import { register } from 'register-service-worker';
 
 register(process.env.SERVICE_WORKER_FILE, {
-  ready () {
+  ready() {
     console.log('App is being served from cache by a service worker.')
   },
-  registered (registration) { // registration -> a ServiceWorkerRegistration instance
+  registered(registration) { // registration -> a ServiceWorkerRegistration instance
     console.log('Service worker has been registered.')
   },
-  cached (registration) { // registration -> a ServiceWorkerRegistration instance
+  cached(registration) { // registration -> a ServiceWorkerRegistration instance
     console.log('Content has been cached for offline use.')
   },
-  updatefound (registration) { // registration -> a ServiceWorkerRegistration instance
+  updatefound(registration) { // registration -> a ServiceWorkerRegistration instance
     console.log('New content is downloading.')
   },
-  updated (registration) { // registration -> a ServiceWorkerRegistration instance
+  updated(registration) { // registration -> a ServiceWorkerRegistration instance
     console.log('New content is available; please refresh.')
   },
-  offline () {
+  offline() {
     console.log('No internet connection found. App is running in offline mode.')
   },
-  error (err) {
+  error(err) {
     console.error('Error during service worker registration:', err)
   }
 })
-
-// ServiceWorkerRegistration: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
